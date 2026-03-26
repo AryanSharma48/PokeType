6bef791f346a28f323eec9bbe5fc6395eb30e1f5
Aryan Sharma <aryansharma24106@gmail.com>
2026-03-26 21:53:53 +0530

    fix deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/The-Typist/",
+  // base:"/The-Typist/", // Removed for Vercel deployment, as it deploys to the root by default
   resolve:{
     alias:{
       "@": path.resolve(__dirname,"./src"),
