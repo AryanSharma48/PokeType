2a98ab807ba9664f889ca3384c84b9ed43d0167d
Aryan Sharma <aryansharma24106@gmail.com>
2026-03-26 22:05:26 +0530

    fix: import of image in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,11 @@
 import "../styles/Header.css"
+import pokeballImg from "../images/pokeball.png"
 
 export default function Header(){
     return(
         <header>
             <div className="logo">
-                <img src="src/images/pokeball.png" alt="logo" />
+                <img src={pokeballImg} alt="logo" />
                 <span><span className="poke-red">Poké</span>Type</span>
             </div>
         </header>
